a1dae3ddf84fb433ea0eb5825900959161c6117c
kkb <0furiousblade0@gmail.com>
2024-02-13 20:47:01 +0900

    [#4] model 정의

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# 캘린더에 추가할 기능
+
+# 이벤트 (일정)
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    
+
