1288614e8d59ce47ddb03759b45734e4835fd103
kkb <0furiousblade0@gmail.com>
2024-02-15 22:09:01 +0900

    [#6] HTMLCalendar 상속 - 주석 추가

--- cal/utils.py
+++ cal/utils.py
@@ -43,12 +43,17 @@ class Calendar(HTMLCalendar):
     
     def formatmonth(self, withyear=True):
         events = Event.objects.filter(start_time__year = self.year, start_time__month = self.month)
+        # 현재 연도와 월에 해당하는 이벤트를 DB에서 가져와 events 변수에 저장
         cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+        # HTML 테이블로 시작하는 달력 생성
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+        # 현재 연도, 월에 해당하는 달의 이름을 HTML 텍스트로 추가
         cal += f'{self.formatweekheader()}\n'
+        # 테이블의 요일 헤더를 HTML 텍스트로 추가
         
         for week in self.monthdays2calendar(self.year, self.month):
             cal += f'{self.formatweek(week, events)}\n'
+            # 현재 연도, 월에 해당하는 날짜로 주별로 그룹화
         return cal
     
     
